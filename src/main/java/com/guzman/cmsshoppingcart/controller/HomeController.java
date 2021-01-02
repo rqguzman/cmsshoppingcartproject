c0526318a86436d912c369b6052007f5451e30b7
rqguzman <rafael.guzman@outlook.com>
2021-01-02 11:53:03 -0300

    implement a WebConfig file

--- src/main/java/com/guzman/cmsshoppingcart/controller/HomeController.java
+++ src/main/java/com/guzman/cmsshoppingcart/controller/HomeController.java
@@ -3,10 +3,18 @@ package com.guzman.cmsshoppingcart.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/* 
+ * THIS CONTROLLER WAS GENERATED WITH EDUCATIONAL PURPOSES ONLY
+ * it no longer has any function on the final project.
+ * Its was here only to make it possible to compare with the way 
+ *  WebMvcConfigurer works.
+ *  
+ */
+
 @Controller
 public class HomeController {
 
-	@GetMapping("/")
+	@GetMapping("/someotherpage")
 	public String home() {
 
 		return "home";
