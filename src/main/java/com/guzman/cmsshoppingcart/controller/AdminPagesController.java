74e18e300e9288b0850791903d4407e519adfc22
rqguzman <rafael.guzman@outlook.com>
2021-01-02 22:15:36 -0300

    Implement  Add Page

--- src/main/java/com/guzman/cmsshoppingcart/controller/AdminPagesController.java
+++ src/main/java/com/guzman/cmsshoppingcart/controller/AdminPagesController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.guzman.cmsshoppingcart.model.PageRepository;
@@ -32,6 +33,14 @@ public class AdminPagesController {
 		model.addAttribute("pages", pages);
 		
 		return "admin/pages/index";
+	}
+
+	@GetMapping("/add")
+	public String add(@ModelAttribute Page thePage) {
+		
+		// model.addAttribute("page", new Page());
+		
+		return "admin/pages/add";
 	}
 
 }
