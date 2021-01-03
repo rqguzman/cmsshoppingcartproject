339dd237c409365e1576450c8620d72757aaac22
rqguzman <rafael.guzman@outlook.com>
2021-01-03 08:12:17 -0300

    Add post mapping to edit pages

--- src/main/java/com/guzman/cmsshoppingcart/controller/AdminPagesController.java
+++ src/main/java/com/guzman/cmsshoppingcart/controller/AdminPagesController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -72,4 +73,57 @@ public class AdminPagesController {
 		return "redirect:/admin/pages/add";
 	}
 
+	@GetMapping("/edit/{theId}")
+	public String edit(@PathVariable int theId, Model theModel) {
+		
+		Page thePage = pageRepository.getOne(theId);
+		
+		theModel.addAttribute("page", thePage);
+		
+		return "admin/pages/edit";
+	}
+
+	@PostMapping("/edit")
+	public String edit(@Valid Page thePage, BindingResult theResult, RedirectAttributes theAttributes, Model theModel) {
+		
+		Page pageCurrent = pageRepository.getOne(thePage.getId());
+		
+		if (theResult.hasErrors()) {
+			theModel.addAttribute("pageTitle", pageCurrent.getTitle());
+			return "admin/pages/edit";
+		}
+		
+		theAttributes.addFlashAttribute("message", "Page edited");
+		theAttributes.addFlashAttribute("alertClass", "alert-success");
+		
+		String theSlug = thePage.getSlug() == "" ? thePage.getTitle().toLowerCase().replace(" ", "-")
+				: thePage.getSlug().toLowerCase().replace(" ", "-");
+		
+		//Page slugExists = pageRepository.findBySlug(thePage.getId(), theSlug);
+		Page slugExists = pageRepository.findBySlugAndIdNot(theSlug, thePage.getId());
+		
+		if (slugExists != null) {			
+			theAttributes.addFlashAttribute("message", "Slug exists, please choose another one.");
+			theAttributes.addFlashAttribute("alertClass", "alert-danger");
+			theAttributes.addFlashAttribute("page", thePage);
+			
+		} else {
+			thePage.setSlug(theSlug);
+			
+			pageRepository.save(thePage);
+		}
+		
+		return "redirect:/admin/pages/edit/" + thePage.getId();
+	}
+
 }
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/guzman/cmsshoppingcart/model/PageRepository.java
+++ src/main/java/com/guzman/cmsshoppingcart/model/PageRepository.java
@@ -8,4 +8,9 @@ public interface PageRepository extends JpaRepository<Page, Integer> {
 
 	public Page findBySlug(String slug);
 	
+	// @Query("SELECT p FROM Page p WHERE p.id != :theId AND p.slug = :theSlug")
+	// public Page findBySlug(int theId, String theSlug);
+	
+	public Page findBySlugAndIdNot(String theSlug, int theId);
+	
 }
