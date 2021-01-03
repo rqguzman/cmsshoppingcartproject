096e504a17e599f5447b06df8a557e9d72010682
rqguzman <rafael.guzman@outlook.com>
2021-01-03 16:06:53 -0300

    Add get mapping to add new category view

--- src/main/java/com/guzman/cmsshoppingcart/controller/AdminCategoriesController.java
+++ src/main/java/com/guzman/cmsshoppingcart/controller/AdminCategoriesController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.guzman.cmsshoppingcart.model.CategoryRepository;
@@ -26,5 +27,35 @@ public class AdminCategoriesController {
 		theModel.addAttribute("categories", categories);
 		
 		return "admin/categories/index";
+	}
+
+	/*
+	 * the '@ModelAttribute' it's optional.
+	 * It is only useful if, for instance, you need to create an alias for the 
+	 * thymeleaf object (th:object) of the Html tag <form> at the respective 
+	 * view (add.html, in this case).
+	 * It would look like this:
+	 * 
+	 * public String add(@ModelAttribute("admin_cat") Category theCategory) {
+	 * 
+	 * and at Html file:
+	 * 
+	 * <form action="#" th:action="@{/admin/categories/add}" 
+	 * 		th:object="${admin_cat}" method="post">
+	 * 
+	 * There is a much better use for this anottation though:
+	 * If you need an attribute to be available throughout the entire class,
+	 * you might create a class attribute with this annotation:
+	 * 
+	 * 	@ModelAttribute("category")
+	 * 	private Category getCategory() {
+	 * 		return new Category();
+	 * 	}
+	 * 
+	 */
+	@GetMapping("/add")
+	public String add(@ModelAttribute Category theCategory) {
+				
+		return "admin/categories/add";
 	}
 }
