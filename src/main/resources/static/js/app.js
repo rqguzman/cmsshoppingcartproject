573f4b52825c269ac3b717b3b68ea93afdb02e8b
rqguzman <rafael.guzman@outlook.com>
2021-01-05 21:28:25 -0300

    Integrate ckeditor - WYSIWYG editing tool

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,32 +1,35 @@
-/*
-	This code was kept for educational purposes.
-	The course instructor implemented his app with this.
-	I've used the "onclick" functionality instead.
-*/
+$(function () {
 
-$(function() {
+    $("a.confirmDeletion").click(function () {
+        if (!confirm("Confirm deletion")) return false;
+    });
 
-	$("a.confirmDeletion").click(function() {
-		if (!confirm("Confirm deletion")) return false;
-	});
+    if ( $("#content").length) {
+        ClassicEditor
+            .create(document.querySelector("#content"))
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    if ( $("#description").length) {
+        ClassicEditor
+            .create(document.querySelector("#description"))
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
 });
-
-/*
-	Image Preview
-*/
+	
 function readURL(input, idNum) {
-	if(input.files && input.files[0]) {
-	
-		let reader = new FileReader();
-		
-		reader.onload = function (e) {
-		
-			$("#imgPreview" + idNum).attr("src", e.target.result).width(100).height(100);
-		
-		}
-		
-		reader.readAsDataURL(input.files[0]);
-	
-	}
+    if (input.files && input.files[0]) {
+        let reader = new FileReader();
 
+        reader.onload = function (e) {
+            $("#imgPreview" + idNum).attr("src", e.target.result).width(100).height(100);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
 }
