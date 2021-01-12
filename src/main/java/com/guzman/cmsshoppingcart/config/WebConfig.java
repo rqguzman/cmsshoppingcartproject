b101a8f5809c75d88e1c8b2b0dca1d85a0827f3b
rqguzman <rafael.guzman@outlook.com>
2021-01-12 09:00:32 -0300

    Add new Home Page

--- src/main/java/com/guzman/cmsshoppingcart/config/WebConfig.java
+++ src/main/java/com/guzman/cmsshoppingcart/config/WebConfig.java
@@ -2,17 +2,17 @@ package com.guzman.cmsshoppingcart.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-		
-		registry.addViewController("/").setViewName("home");
-	}
+	// kept for educational purposes
+//	@Override
+//	public void addViewControllers(ViewControllerRegistry registry) {
+//		
+//		registry.addViewController("/").setViewName("home");
+//	}
 
 	
 	private static final String[] CLASSPATH_RESOURCE_LOCATIONS =
---
+++ src/main/java/com/guzman/cmsshoppingcart/controller/PagesController.java
@@ -0,0 +1,27 @@
+package com.guzman.cmsshoppingcart.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.guzman.cmsshoppingcart.model.PageRepository;
+import com.guzman.cmsshoppingcart.model.data.Page;
+
+@Controller
+@RequestMapping("/")
+public class PagesController {
+
+	@Autowired
+	private PageRepository pageRepository;
+	
+	@GetMapping
+	public String home(Model theModel) {
+		
+		Page page = pageRepository.findBySlug("home");
+		theModel.addAttribute("page", page);
+		
+		return "page";
+	}
+}
